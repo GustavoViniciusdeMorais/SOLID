fa2eaf7836bcca573e8ec39c23392bea1462d0cc
Gustavo Vinicius <gustavo@appfacilita.com>
2023-01-13 13:49:57 -0300

    tests configuration

---
+++ PHP/tests/HtmlTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Gustavo\Solid;
+
+use PHPUnit\Framework\TestCase;
+
+class HtmlTest extends TestCase
+{
+    public function testSimpleTest()
+    {
+        $this->assertTrue(true);
+    }
+}
